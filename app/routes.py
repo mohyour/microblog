7722ef1e88386613fed72910f08966a2a59dc6a7
mohyour <mohyour01@gmail.com>
2018-11-12 09:02:33 +0100

    template inheritance

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,15 @@ def home():
     user = {
         'username': 'Moyosore'
     }
-    return render_template('index.html', user=user, title='Homepage')
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', user=user, posts=posts)
