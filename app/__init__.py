7a1f3bce050e41dc80810f0fae4a6f5a8fabe14f
mohyour <mohyour01@gmail.com>
2018-11-12 11:06:31 +0100

    include login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from app.config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # import at the bottom to prevent circular import
 from app import routes  # noqa: E402
