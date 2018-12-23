2ea9fe3711a4bcdfb351de6bfe45d6aaa51844ad
mohyour <mohyour01@gmail.com>
2018-12-23 23:49:47 +0100

    implement user login

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from app.config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 # import at the bottom to prevent circular import
 from app import routes, models  # noqa: E402
