551025d2468bf1cf190dd7a7bd4b7a726c37abe1
mohyour <mohyour01@gmail.com>
2018-12-23 22:11:03 +0100

    include database

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Flask Microblog
 
-.env example:
+.flaskenv example:
 ```
 FLASK_APP=microblog.py
+FLASK_DEBUG=True
+SECRET_KEY=<secret-key>
+DATABASE_URL=<database-name>
 ```
+Database Migration
+
+`flask db init` - creates database migration folder
+`flask db migrate` - Generates automatic migration script
+`flask db upgrade` - Apply database migration
--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from app.config import Config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # import at the bottom to prevent circular import
-from app import routes  # noqa: E402
+from app import routes, models  # noqa: E402
